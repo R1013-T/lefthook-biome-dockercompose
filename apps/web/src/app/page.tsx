4d3628fc04aa9bbe62e7bbaca5042cde965ffc37
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-10 19:12:54 +0900

    feat: done

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className='grid place-content-center'>
       <div>lefthook + docker compose</div>
       <p>biome, commitlint, commitizen</p>
     </main>
